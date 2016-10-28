2212ad599cc53540e14059fbeff97816cf1de84f
Zotov Igor <basterbod@gmail.com>
2016-10-28 13:14:03 +0300

    no message

---
+++ Source/Escape_4125/Escape_4125.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Escape_4125.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Escape_4125, "Escape_4125" );
---
+++ Source/Escape_4125/Escape_4125GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Escape_4125GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_4125_API AEscape_4125GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
