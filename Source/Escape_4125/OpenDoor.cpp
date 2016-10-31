31daa6e67e78cf2ec7e4985b708e23e4729269b8
Zotov Igor <ultimazer@github.com>
2016-10-31 18:56:30 +0200

    Add Open door  with trigger

--- Source/Escape_4125/OpenDoor.cpp
+++ Source/Escape_4125/OpenDoor.cpp
@@ -21,6 +21,12 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+	
+}
+
+void UOpenDoor::OpenDoor()
+{
 	//Find the owning Actor
 	AActor* Owner = GetOwner();
 
@@ -29,7 +35,6 @@ void UOpenDoor::BeginPlay()
 
 	//Set the door rotation
 	Owner->SetActorRotation(NewRotation);
-	
 }
 
 
@@ -38,6 +43,11 @@ void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompo
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// ...
+	// Pol the Trigger Volume
+	//If the ActorThatOpens is in the volume
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens)) {
+		OpenDoor();
+	}
+	
 }
 
