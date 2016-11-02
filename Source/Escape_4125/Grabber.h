218d0e48149127fe981f90fbb84d862b501998c3
Zotov Igor <ultimazer@github.com>
2016-11-02 15:38:08 +0200

    Add a method line-cast from player and detect object look at

---
+++ Source/Escape_4125/Grabber.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ESCAPE_4125_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+private:
+	//How far ahead of the player can we reach in cm
+	UPROPERTY(VisibleAnywhere)
+	float Reach = 100.0f;
+	
+};
